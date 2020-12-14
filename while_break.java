03d46fe136a64ba1f0996221d5d180cb61f921a8
qa191120 <74743291+qa191120@users.noreply.github.com>
2020-12-14 20:03:53 +0200

    Update while_break.java

--- while_break.java
+++ while_break.java
@@ -30,16 +30,39 @@ public class Main
         }
         while (counter <= 5);
         System.out.println(String.format("You had %d", counter));
-        
+
         //-- targil:
         // class has 10 students
         // write program which inputs 10 grades
-        // if negative grade was inserted, or grade > 100 inserted 
+        // if negative grade was inserted, or grade > 100 inserted
         //           then break
-        // *etgar: if 5 students got 100 then break because 
+        // *etgar: if 5 students got 100 then break because
         //          this is super class
         // **etgar: calculate the average
-        
+        // ***etgar: calculate the average for each grade (danger!)
+
+
+        int students_counter = 1;
+        int counter_100 = 0;
+        int grade;
+        do {
+            System.out.println("Enter grade:");
+            grade = s.nextInt();
+            if (grade < 0 || grade > 100) {
+                System.out.println("Grade not in range! exit...");
+                break;
+            }
+            if (grade == 100) {
+                counter_100++;
+                if (counter_100 == 5) {
+                    System.out.println("Super star class!");
+                    break;
+                }
+            }
+            
+            students_counter++;
+        }
+        while (students_counter <= 10);
 
         System.out.println("Goodbye");
     }
